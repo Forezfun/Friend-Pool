cd8d43ea163f2afdd1138743f9f8cf28ba1ef09a
Forezfun <windowspls@mail.ru>
2025-09-12 21:31:17 +1000

    delete vizo

--- app/client-side/src/app.routes.ts
+++ app/client-side/src/app.routes.ts
@@ -1,9 +1,4 @@
 import { Routes } from '@angular/router';
-import { PlayerPage } from './pages/player-page/player-page';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component:PlayerPage
-    }
 ];
